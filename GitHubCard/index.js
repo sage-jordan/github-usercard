ebb50088fbe226ee0a3ac4d0417a6d84cd013463
sage-jordan <sagemjrodan@gmail.com>
2019-07-18 23:01:46 -0400

    step 3 done i think

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,13 +1,16 @@
+import { create } from "domain";
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-const axios = require('axios');
-axios.get('https://api.github.com/users/sagejordan')
-  .then(function (response){
+// const axios = require('axios');
+axios.get('https://api.github.com/users/sage-jordan')
+  .then(response=>{
     console.log(response);
+    return createThisThing(response);
   })
-  .catch(function (err) {
+  .catch(err=>{
     console.log(err);
   })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -50,9 +53,52 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
-
+function createThisThing (obj) {
+  // <div class="card">
+  const card = document.createElement('div');
+  card.classList.add('card');
+  // <img src={}/>
+  const image = document.createElement('img');
+  image.src = 'https://avatars1.githubusercontent.com/u/50002551?v=4';
+  card.appendChild(image);
+  // <div class="card-info">
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  card.appendChild(cardInfo);
+  // <h3 class="name">Sage Jordan</h3>
+  const h3Name = document.createElement('h3');
+  h3Name.textContent = `${obj.name}`;
+  cardInfo.appendChild(h3Name);
+  // <p class="username">sage-jordan</p>
+  const userName = document.createElement('p');
+  userName.classList.add('username');
+  cardInfo.appendChild(userName);
+  // <p>Location: {users location}</p>
+  const locaion = document.createElement('p');
+  location.textContent = `Location: ${obj.location}`;
+  cardInfo.appendChild(location);
+  // <p>Profile:
+  const profile = document.createElement('p');
+  cardInfo.appendChild(profile);
+  // <a href={address to users github page}>{address to users github page}</a>
+  const githubHTML = document.createElement('a');
+  githubHTML.href = obj.url;
+  githubHTML.textContent = obj.url;
+  profile.appendChild(githubHTML);
+  // <p>Followers: {users followers count}</p>
+  const followers = document.createElement('p');
+  followers.textContent = `Followers: ${obj.followers}`;
+  cardInfo.appendChild(followers);
+  // <p>Following: {users following count}</p>
+  const following = document.createElement('p');
+  following.textContent = `Following: ${obj.following}`;
+  cardInfo.appendChild(following);
+  // <p>Bio: {users bio}</p>
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${obj.bio}`;
+  cardInfo.appendChild(bio);
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
