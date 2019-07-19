b137a8363be1c3f60b370961ec7c17e234edc945
sage-jordan <sagemjrodan@gmail.com>
2019-07-18 21:16:43 -0400

    started

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+const axios = require('axios');
+axios.get('https://api.github.com/users/sagejordan')
+  .then(function (response){
+    console.log(response);
+  })
+  .catch(function (err) {
+    console.log(err);
+  })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
